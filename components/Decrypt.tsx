2b14b4655b62d753f49a6ed05cbc07d4abb07002
gtk <gatikbahet041211@gmail.com>
2025-12-29 00:59:34 +0530

    Update Decrypt.tsx

---
+++ components/Decrypt.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Decrypt: React.FC = () => {
+  return (
+    <div className="p-4 bg-gray-800 rounded my-4 text-white">
+      <h2 className="font-bold mb-2">Decrypt File</h2>
+      <p>UI and crypto logic will go here.</p>
+    </div>
+  );
+};
+
+export default Decrypt;
