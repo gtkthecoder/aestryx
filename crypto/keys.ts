b2ddc0bc73ba72f17efc438264c8ba3ac2d76593
gtk <gatikbahet041211@gmail.com>
2025-12-29 01:05:42 +0530

    Update keys.ts

---
+++ crypto/keys.ts
@@ -0,0 +1,4 @@
+export const generateRSAKeys = async () => {
+  // TODO: RSA-4096 key pair using Web Crypto API
+  return { publicKey: 'public-key-placeholder', privateKey: 'private-key-placeholder' };
+};
